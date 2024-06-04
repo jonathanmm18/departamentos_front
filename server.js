3f705e99ae4d72b89217dd857a662e14c991e854
Harry <harry@users-MacBook-Pro.local>
2024-06-03 19:19:21 -0500

    express

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import express from 'express';
+import path from 'path';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import {  fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
